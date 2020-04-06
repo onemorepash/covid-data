606183c79985cc030118823f661f0c055b36d3df
Pavel Lunin <plunin@scaleway.com>
2020-04-07 01:21:33 +0200

    First public release

---
+++ scripts/crawl-worldometers.info/crawl_worldometers_countries_list.py
@@ -0,0 +1,56 @@
+
+import shutil
+import tempfile
+import json
+import re
+
+from urllib.request     import  Request, urlopen
+from urllib.error       import  URLError, HTTPError
+from lxml               import  html, etree
+
+import crawl_wmeter_definitions as definitions
+
+print ('Trying to craws ' + definitions.wmeter_countries_list_url_prefix + ' ... ', end='')
+
+# Connect to worldometers.info and save the coronavirus title page to a temporary file
+request = Request(definitions.wmeter_countries_list_url_prefix, headers={'User-Agent': definitions.wmeter_user_agent})
+
+try:
+        response = urlopen(request)
+except HTTPError as http_error:
+        print("Can't get the countries list from " + definitions.wmeter_countries_list_url_prefix)
+        print('HTTP Error code: ', http_error.code)
+except URLError as http_error:
+        print("Can't connect to " + definitions.wmeter_countries_list_url_prefix)
+        print('Reason: ', http_error.reason)
+else:
+    print ( ' success.' )
+
+    # Put the HTML page in a temporary file
+    with tempfile.NamedTemporaryFile(delete=True) as countries_list_tmp_file:
+        shutil.copyfileobj(response, countries_list_tmp_file)
+
+        # tmp file was opened to write into it in binary mode, so we can't read characters from it
+        # opening it in character mode to pass to the parser
+        with open(countries_list_tmp_file.name, encoding="utf8") as countries_list_html:
+
+            # Parse HTML and create a list of dictionatries [ {'name' : <country name>, "url" : <country page url> } ]
+            html_parser     = etree.HTMLParser()
+            html_tree       = etree.parse(countries_list_html, parser = html_parser)
+
+            # HTML parsing black magic is in the definitions module
+            country_links   = definitions.get_country_links(html_tree)
+
+            country_list    = []
+
+            for country_i in country_links:
+                country             = {}
+                country['cname']    = definitions.country_cname_from_href( country_i.attrib['href'] )
+                country['name']     = country_i.text
+                country['url']      = definitions.wmeter_countries_list_url_prefix + country_i.attrib['href']
+
+                country_list.append(country)
+
+            # Dump the list to a json file
+            with open(definitions.countries_list_json_filename, 'w', encoding='utf8') as countries_list_json_file:
+                json.dump(country_list, countries_list_json_file, indent=4)
---
+++ scripts/crawl-worldometers.info/parse-worldometers-countries.py
@@ -0,0 +1,50 @@
+import crawl_wmeter_definitions as definitions
+
+import shutil
+import tempfile
+import json
+
+from urllib.request import  Request, urlopen
+from urllib.error   import  URLError, HTTPError
+from lxml           import  html, etree
+from jsonfinder     import  jsonfinder
+
+# Get the countries list from json
+
+with open(definitions.countries_list_json_filename, encoding='utf8') as countries_list_json_file:
+    country_list = json.load(countries_list_json_file)
+
+for country in country_list:
+    request = Request( country['url'], headers={'User-Agent': definitions.wmeter_user_agent} )
+
+    print ('Trying to get ' + country['name'] + ' data... ', end='')
+
+    try:
+            response = urlopen(request)
+    except HTTPError as http_error:
+            print("Can't get the countries list from " + country['url'])
+            print('HTTP Error code: ', http_error.code)
+    except URLError as http_error:
+            print("Can't connect to " + country['url'])
+            print('Reason: ', http_error.reason)
+    else:
+        print ("success.")
+
+        with tempfile.NamedTemporaryFile(delete=True) as country_tmp_file:
+            shutil.copyfileobj(response, country_tmp_file)
+
+            # tmp file was opened to write into it in binary mode, so we can't read characters from it
+            # opening it in character mode to pass to the parser
+            with open(country_tmp_file.name, encoding="utf8") as country_page_html:
+                print ('Parsing ' + country['name'])
+
+                html_parser     = etree.HTMLParser()
+                html_tree       = etree.parse(country_page_html, parser = html_parser)
+
+                country_data    = definitions.get_country_data_cumulative_linear( html_tree )
+
+                country_data_filename = definitions.data_dir + country['cname'] + definitions.countries_datafiles_extention
+                print ('Writing output data to ', country_data_filename)
+
+                with open(country_data_filename, 'w', encoding='utf8') as country_data_file:
+                    definitions.create_gnuplot_data(country_data, country_data_file)
